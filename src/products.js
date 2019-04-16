052c75a410b396cd52acb563667c7597d1ece11e
Emery Valencia <emeryvalencia@gmail.com>
2019-04-15 19:55:45 -0700

    Updated products.html, added products.js, renamed main.html to index.html

---
+++ src/products.js
@@ -0,0 +1,8 @@
+/*
+    Product page JavaScript code: consists of all functionality for the 
+    products page.
+*/
+
+function dropdown() {
+    document.getElementById("pet-dropdowns").classList.toggle("show");
+}
