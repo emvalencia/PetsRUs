fe57a69f15252325809436292dd0352b8ef2623d
Emery Valencia <emeryvalencia@gmail.com>
2019-04-16 23:54:35 -0700

    Added all pet product pages, updated navbar, added error page

--- src/product-pages/products.js
+++ src/product-pages/products.js
@@ -15,18 +15,23 @@ function handleClick(element) {
         else if (element == 'reptile') window.location.href = "reptile.html";
 
         /* switch to dog pages */
-        else if (element == 'dog-food') window.location.href = "dog-food.html";
-        else if (element == 'dog-toy') window.location.href = "dog-toy.html";
-        else if (element == 'dog-harness') window.location.href = "dog-harness.html";
-        else if (element == 'dog-treat') window.location.href = "dog-treat.html";
+        else if (element == 'dog-food') window.location.href = "dog-pages/dog-food.html";
+        else if (element == 'dog-toy') window.location.href = "dog-pages/dog-toy.html";
+        else if (element == 'dog-harness') window.location.href = "dog-pages/dog-harness.html";
+        else if (element == 'dog-treat') window.location.href = "dog-pages/dog-treat.html";
 
         /* switch to cat pages */
-        else if (element == 'cat-food') window.location.href = "cat-food.html";
-        else if (element == 'cat-playground') window.location.href = "cat-playground.html";
-        else if (element == 'cat-carrier') window.location.href = "cat-carrier.html";
+        else if (element == 'cat-food') window.location.href = "cat-pages/cat-food.html";
+        else if (element == 'cat-playground') window.location.href = "cat-pages/cat-playground.html";
+        else if (element == 'cat-carrier') window.location.href = "cat-pages/cat-carrier.html";
 
         /* switch to reptile pages */
+        else if (element == 'reptile-food') window.location.href = "reptile-pages/reptile-food.html";
+        else if (element == 'reptile-habitat') window.location.href = "reptile-pages/reptile-habitat.html";
+        else if (element == 'reptile-meds') window.location.href = "reptile-pages/reptile-meds.html";
         
+        /* else */
+        // window.location.href = "../error-page.html";
     }
 
 }
