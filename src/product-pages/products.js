a92b208ab1bad965a64a4230fa4044111ffa6e60
Emery Valencia <emeryvalencia@gmail.com>
2019-04-16 22:38:39 -0700

    Added cat and reptile general pages

--- src/product-pages/products.js
+++ src/product-pages/products.js
@@ -9,16 +9,23 @@ function handleClick(element) {
     else {
         console.log(element);
 
-        /* switch to pet page */
+        /* switch to main pet page */
         if (element == 'dog') window.location.href = "dog.html";
         else if (element == 'cat') window.location.href = "cat.html";
         else if (element == 'reptile') window.location.href = "reptile.html";
 
-        /* switch to product pages */
+        /* switch to dog pages */
         else if (element == 'dog-food') window.location.href = "dog-food.html";
         else if (element == 'dog-toy') window.location.href = "dog-toy.html";
         else if (element == 'dog-harness') window.location.href = "dog-harness.html";
         else if (element == 'dog-treat') window.location.href = "dog-treat.html";
+
+        /* switch to cat pages */
+        else if (element == 'cat-food') window.location.href = "cat-food.html";
+        else if (element == 'cat-playground') window.location.href = "cat-playground.html";
+        else if (element == 'cat-carrier') window.location.href = "cat-carrier.html";
+
+        /* switch to reptile pages */
         
     }
 
